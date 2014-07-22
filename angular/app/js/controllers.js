fd5d1844eb1da9e92a0be491a301769d5180a5bf
Vishwajeet Vatharkar <vishwajeetvatharkar@gmail.com>
2014-07-23 00:03:49 +0530

    Login with AngularJS

--- angular/app/js/controllers.js
+++ angular/app/js/controllers.js
@@ -4,14 +4,13 @@
 
 angular.module('CI_signup_login.controllers', [])
   .controller('LoginCtrl', ['$scope','LoginService', function($scope, LoginService) {
-        $scope.logIn = function () {
+        $scope.login = function () {
 
-
-
-            LoginService.loginUser($scope.email, $scope.password).then(function (user) {
-                alert("HI");
+            LoginService.loginUser($scope.email, $scope.password)
+                .then(function (user) {
+            alert("Success");
             },function(error) {
-
+                alert("Failed");
             });
         };
   }]);
--- angular/app/js/services.js
+++ angular/app/js/services.js
@@ -6,18 +6,26 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('CI_signup_login.services', [])
-    .service('userService', function($http,$q)
+    .service('LoginService', function($http,$q)
     {
-        this.loginUser = function()
+        this.loginUser = function(email,password)
         {
             var deferred = $q.defer();
-            $http({method: 'POST', url: 'UserService/userLogin.json'}).
+            $http({method: 'POST', url: 'UserService/userLogin.json', data:{email:email,password:password}}).
                 success(function(data, status, headers, config) {
-                    deferred.resolve(data);
-                }).
+
+                    if(data.status == "success")
+                    {
+                        deferred.resolve(data);
+                    }
+                    else
+                    {
+                        deferred.reject();
+                    }
+                    }).
                 error(function(data, status, headers, config) {
                     deferred.reject();
                 });
-
+            return deferred.promise;
         }
     });
